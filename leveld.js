13b8c37bdd19a0c7f244e664650980fc0b8da981
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-07 15:10:50 +0800

    Updated build.

--- leveld.js
+++ leveld.js
@@ -35,6 +35,9 @@
 			"module": "leveld",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/leveld.git",
 			"test": "leveld-test.js",
 			"global": true
