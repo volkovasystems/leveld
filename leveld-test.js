1a16d2c3613cc4018e9cbd554e5d4b8c72ef6211
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-25 13:30:41 +0800

    Updated test.

--- leveld-test.js
+++ leveld-test.js
@@ -1,32 +1,43 @@
 const assert = require( "assert" );
 const leveld = require( "./leveld.js" );
 
-assert.deepEqual( leveld( [ [ 1 ] ], 1 ), [ [ 1 ] ], "should be equal" );
+assert.deepEqual( leveld( [ [ [ 1 ] ] ], 2 ), [ [ 1 ] ], "should return [ [ 1 ] ]" );
 
-assert.deepEqual( leveld( [ [ [ 1 ] ] ], 2 ), [ [ 1 ] ], "should be equal" );
+assert.deepEqual( leveld( [ [ [ 1 ] ] ], 3 ), [ 1 ], "should return [ 1 ]" );
 
-assert.deepEqual( leveld( [ [ [ 1 ] ] ], 3 ), [ 1 ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 2 ),
+	[ [ [ 1, 2, 3 ],[ [ [ 4, 5, 6 ] ] ] ] ],
+	"should return [ [ [ 1, 2, 3 ],[ [ [ 4, 5, 6 ] ] ] ] ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 2 ), [ [ [ 1, 2, 3 ],[ [ [ 4, 5, 6 ] ] ] ] ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 3 ),
+	[ [ 1, 2, 3 ], [ [ [ 4,5,6 ] ] ] ],
+	"should return [ [ 1, 2, 3 ], [ [ [ 4,5,6 ] ] ] ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 3 ), [ [ 1, 2, 3 ], [ [ [ 4,5,6 ] ] ] ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 4 ),
+	[ 1, 2, 3, [ [ 4, 5, 6 ] ] ], "should return [ 1, 2, 3, [ [ 4, 5, 6 ] ] ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 4 ), [ 1, 2, 3, [ [ 4, 5, 6 ] ] ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 5 ),
+	[ 1, 2, 3, [ 4, 5, 6 ] ], "should return [ 1, 2, 3, [ 4, 5, 6 ] ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 5 ), [ 1, 2, 3, [ 4, 5, 6 ] ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 6 ),
+	[ 1, 2, 3, 4, 5, 6 ], "should return [ 1, 2, 3, 4, 5, 6 ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 6 ), [ 1, 2, 3, 4, 5, 6 ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 7 ),
+	[ 1, 2, 3, 4, 5, 6 ], "should return [ 1, 2, 3, 4, 5, 6 ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], 7 ), [ 1, 2, 3, 4, 5, 6 ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]] ),
+	[ [ [ 1, 2, 3 ],[ [ [ 4, 5, 6 ] ] ] ] ], "should return [ [ [ 1, 2, 3 ],[ [ [ 4, 5, 6 ] ] ] ] ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]] ), [ [ [ 1, 2, 3 ],[ [ [ 4, 5, 6 ] ] ] ] ], "should be equal" );
+assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], Infinity ),
+	[ 1, 2, 3, 4, 5, 6 ], "should return [ 1, 2, 3, 4, 5, 6 ]" );
 
-assert.deepEqual( leveld( [[[[1,2,3],[[[4,5,6]]]]]], Infinity ), [ 1, 2, 3, 4, 5, 6 ], "should be equal" );
+assert.deepEqual( leveld( [[1,2,3],[3,4,5]], 2 ),
+	[ 1, 2, 3, 3, 4, 5 ], "should return [ 1, 2, 3, 3, 4, 5 ]" );
 
-assert.deepEqual( leveld( [[1,2,3],[3,4,5]], 2 ), [ 1, 2, 3, 3, 4, 5 ], "should be equal" );
+assert.deepEqual( leveld( [[[1,2,3],[3,4,5]],[4,5,6]], 2 ),
+	[ [ 1, 2, 3 ], [ 3, 4, 5 ], 4, 5, 6 ], "should return [ [ 1, 2, 3 ], [ 3, 4, 5 ], 4, 5, 6 ]" );
 
-assert.deepEqual( leveld( [[[1,2,3],[3,4,5]],[4,5,6]], 2 ), [ [ 1, 2, 3 ], [ 3, 4, 5 ], 4, 5, 6 ], "should be equal" );
-
-assert.deepEqual( leveld( [[1,2,3],[3,4,5]] ), [ 1, 2, 3, 3, 4, 5 ], "should be equal" );
+assert.deepEqual( leveld( [[1,2,3],[3,4,5]] ),
+	[ 1, 2, 3, 3, 4, 5 ], "should return [ 1, 2, 3, 3, 4, 5 ]" );
 
 console.log( "ok" );
